9ebf86e84e41341bdbd67baad674aba07fb91a30
juliangoes89 <juliangomezes89@gmail.com>
2020-03-14 15:20:15 -0500

    Primer Commit

---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html'
+})
+export class HijoComponent implements OnInit {
+  @Input() contador:number;
+  @Output() cambioContador = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  multiplicar(){
+    this.contador *=2;
+    this.cambioContador.emit(this.contador);
+  }
+
+  dividir(){
+    this.contador /=2;
+    this.cambioContador.emit(this.contador);
+  }
+  resetNieto(event:any){
+    this.contador = event;
+    this.cambioContador.emit(this.contador);
+  }
+}
